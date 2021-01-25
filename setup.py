bebcf13b417459d588e6b1a8483af89c591f1671
Marius Merschformann <marius.merschformann@gmail.com>
2021-01-25 19:55:53 +0100

    Adding password generator script

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+setuptools.setup(
+    name="parole",
+    description="A simple password generator",
+    version="0.0.1",
+    author="Marius Merschformann",
+    author_email="marius.merschformann@gmail.com",
+    url="https://github.com/merschformann/parole",
+    packages=setuptools.find_packages(),
+    install_requires=[],
+    entry_points={
+        "console_scripts": ["parole=src.generate:main"],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Environment :: Console",
+    ],
+)
